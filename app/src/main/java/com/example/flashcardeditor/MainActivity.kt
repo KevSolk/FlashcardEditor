c33e9ff64ac09bd838bc5af19fa739f2a2e7bde0
Kevin <KevSolk@users.noreply.github.com>
2021-01-24 18:09:37 -0700

    Nav Bar Unstaged files
    Some files were not staged

--- app/src/main/java/com/example/flashcardeditor/MainActivity.kt
+++ app/src/main/java/com/example/flashcardeditor/MainActivity.kt
@@ -1,20 +1,34 @@
-package com.example.flashcardeditor
+ package com.example.flashcardeditor
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val appBar = findViewById<Toolbar>(R.id.toolbar)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navView = findViewById<NavigationView>(R.id.nav_drawer)
+        appBar.setNavigationOnClickListener{
+            drawerLayout.open()
         }
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            drawerLayout.close()
+            true
+        }
+
     }
 }
